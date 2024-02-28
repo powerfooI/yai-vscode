fc91e16d30e8dfba836a833c1214a72d4bf40e6f
Wangshuyi <whiteffire@163.com>
2024-02-29 01:08:26 +0800

    feat(go): import golang module

--- src/languages/golang.ts
+++ src/languages/golang.ts
@@ -26,14 +26,99 @@ export async function golangHandler(context: vscode.ExtensionContext, ws: vscode
   if (!module) {
     return
   }
-  
+
   const edit = new vscode.WorkspaceEdit()
   const alias = await vscode.window.showInputBox({ title: 'Input the alias of the importing package, original name by default' })
-  // 1. not exist, insert directly; no import statement
-  // 2. exist, compare the alias; from import 'xxx' to import ( ... )
-  // 2.1 same, do nothing
-  // 2.2 different, replace the alias or throw an error
-  // TODO: implement the edit logic
+
+  const singleImportPattern = /import (.*"\S+")/g
+  const multiImportPattern = /import \([\s\S]+?\)/g
+  const packagePattern = /package (\S+)/g
+
+  const docText = document.getText().replace(/\/\/.+/g, '//')
+  const lines = docText.split('\n').map((l) => l.trim())
+
+  const packageMatch = docText.match(packagePattern)
+  const singleImportMatch = docText.match(singleImportPattern)
+  const multiImportMatch = docText.match(multiImportPattern)
+
+  if (!packageMatch) {
+    vscode.window.showErrorMessage('No package statement found in the file')
+    return
+  }
+
+  let packageLineNo = 0       // line no of package xxx
+  let importLineNo = 0        // line no of import [alias] "mode"
+  let importStartLineNo = 0   // line no of import (
+  let importEndLineNo = 0     // line no of ), pairing with importStartLineNo
+
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].match(packagePattern)) {
+      packageLineNo = i
+      break
+    }
+  }
+  const insertPart = alias ? `${alias} "${module}"` : `"${module}"`
+
+  if (!singleImportMatch && !multiImportMatch) {
+    /**
+     * No import statement
+     */
+    edit.insert(document.uri, new vscode.Position(packageLineNo + 1, 0), `\nimport ${insertPart}\n`)
+  } else if (singleImportMatch) {
+    /**
+     * Single import statement like,
+     * import "fmt"
+     * import f "fmt"
+     */
+    const targetModIndex = singleImportMatch[0].indexOf(`"${module}"`)
+    if (targetModIndex < 0) {
+      // no target module
+      for (let i = 0; i < lines.length; i++) {
+        if (lines[i].match(singleImportPattern)) {
+          importLineNo = i
+          break
+        }
+      }
+      const instStatement = `import (\n\t${singleImportMatch[0].replace('import ', '')}\n\t${insertPart}\n)`
+      edit.replace(document.uri, new vscode.Range(new vscode.Position(importLineNo, 0), new vscode.Position(importLineNo + 1, 0)), instStatement)
+    } else {
+      vscode.window.showInformationMessage('The module has been imported')
+    }
+  } else if (multiImportMatch) {
+    /**
+     * Multiple import statement like,
+     * import (
+     *    "fmt"
+     *   o "os"
+     * )
+     */
+    const targetModIndex = multiImportMatch[0].indexOf(`"${module}"`)
+    if (targetModIndex < 0) {
+      // no target module
+      for (let i = 0; i < lines.length; i++) {
+        if (lines[i].includes("import (")) {
+          importStartLineNo = i
+        }
+        if (lines[i].includes(")")) {
+          importEndLineNo = i
+          break
+        }
+      }
+
+      const instStatement = `\t${insertPart}\n`
+      console.log(importStartLineNo, importEndLineNo, instStatement)
+      edit.insert(document.uri, new vscode.Position(importStartLineNo + 1, 0), instStatement)
+    } else {
+      vscode.window.showInformationMessage('The module has been imported')
+    }
+  }
+
+  const ok = await vscode.workspace.applyEdit(edit)
+  if (ok) {
+    vscode.window.showInformationMessage('Import successfully!')
+  } else {
+    vscode.window.showErrorMessage('Fail to import the module')
+  }
 }
 
 function getDepsFromGoMod(goModPath: string): Dependency[] {
